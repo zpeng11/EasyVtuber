3d230712ed414a92ecdf2a7a53e20425796f58f3
zpeng11 <a2843173630@gmail.com>
2025-01-10 22:28:42 -0500

    Package for release

---
+++ 01C.执行打包.bat
@@ -0,0 +1,41 @@
+@echo on
+cd /D "%~dp0"
+
+if not exist "envs" mkdir envs
+if not exist "envs\miniconda3" mkdir envs\miniconda3
+
+IF not EXIST %~dp0envs\miniconda3\Scripts (
+    @RD /S /Q %~dp0envs\miniconda3
+    mkdir %~dp0envs\miniconda3
+    echo "Downloading miniconda..."
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo.anaconda.com/miniconda/Miniconda3-py310_24.9.2-0-Windows-x86_64.exe', '.\envs\miniconda3.exe')"
+
+    echo "Installling minconda..."
+    start /wait "" %~dp0envs\miniconda3.exe /S /AddToPath=0 /RegisterPython=0 /InstallationType=JustMe /D=%~dp0envs\miniconda3
+    echo "Successfully install minconda"
+    del envs\miniconda3.exe
+)
+
+SET PATH=%~dp0envs\miniconda3\Scripts;%PATH%
+
+call activate
+call conda env list
+call conda update -y --all
+call conda create -y -n ezvtb_rt_venv python=3.10
+call conda activate ezvtb_rt_venv
+call conda env list
+
+call conda install -y pycuda -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/ 
+
+call conda install -y conda-pack -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/ 
+
+call python -m pip install --upgrade pip wheel -i https://mirrors.aliyun.com/pypi/simple/
+
+call conda-pack -n ezvtb_rt_venv -o %~dp0envs\python_embedded.zip --format zip
+
+call python -m pip wheel nvidia-cudnn-cu12 -i https://mirrors.aliyun.com/pypi/simple/ -w envs\wheels
+call pip wheel tensorrt_cu12_libs==10.6.0 tensorrt_cu12_bindings==10.6.0 tensorrt==10.6.0 --extra-index-url https://pypi.nvidia.com -w envs\wheels
+call python -m pip wheel -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/ -w envs\wheels
+
+@REM Up to here you can use the envs\python_embedded.zip and envs\wheels to reconstruct a new environment
+pause
--- 02B.启动器（调试输出）.bat
+++ 02B.启动器（调试输出）.bat
@@ -1,12 +1,12 @@
 @echo on
 cd /D "%~dp0"
-IF EXIST %~dp0envs\miniconda3\Scripts SET PATH=%~dp0envs\miniconda3\Scripts;%PATH%
-call activate
-call conda env list
-call conda activate ezvtb_rt_venv
-call conda env list
 
-IF not EXIST %~dp0launcher.json call python ezvtb_rt_interface.py
+SET PATH=%~dp0envs\python_embedded;%~dp0envs\python_embedded\Scripts;%PATH%
+
+IF not EXIST %~dp0launcher.json (
+    call pip install --no-index --find-links %~dp0envs\wheels  -r requirements-pack.txt 
+    call python ezvtb_rt_interface.py
+)
 
 python launcher2.py
 
